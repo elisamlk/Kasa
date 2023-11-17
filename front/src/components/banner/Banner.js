f63873a52b070dae63ef5b02449728f1273719ae
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-11-17 15:37:38 +0100

    Mise en place du Banner

---
+++ front/src/components/banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.scss";
+
+function Banner() {
+  return (
+    <section className="banner-section">
+      <h1>Chez vous, partout et ailleurs</h1>
+    </section>
+  );
+}
+
+export default Banner;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Banner from "../components/banner/Banner";
 
 function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <main>
+      <Banner />
+    </main>
   );
 }
 
