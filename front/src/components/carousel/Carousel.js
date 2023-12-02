d68243943855525d4efc585dd427fe86b985d772
agence136 <elisabeth.malek@agence136.fr>
2023-12-02 19:23:58 +0100

    Fix probleme carousel

--- front/src/components/carousel/Carousel.js
+++ front/src/components/carousel/Carousel.js
@@ -6,31 +6,34 @@ import "./Carousel.scss";
 
 const Carousel = (props) => {
   const pictures = props;
-  console.log("1=>",pictures)
-
+  const listPictures = props.pictures;
+  console.log("1=>", pictures);
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const goToPrevSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+      prevIndex === 0 ? listPictures.length - 1 : prevIndex - 1
     );
   };
-
   const goToNextSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+      prevIndex === listPictures.length - 1 ? 0 : prevIndex + 1
     );
   };
-
   // console.log(pictures);
-
   return (
     <div className="carousel">
-      <FontAwesomeIcon className="carousel-btn prev" icon={faChevronLeft} onClick={goToPrevSlide} />
-      <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
-      <FontAwesomeIcon className="carousel-btn next" icon={faChevronRight} onClick={goToNextSlide} />
+      <FontAwesomeIcon
+        className="carousel-btn prev"
+        icon={faChevronLeft}
+        onClick={goToPrevSlide}
+      />
+      <img src={listPictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <FontAwesomeIcon
+        className="carousel-btn next"
+        icon={faChevronRight}
+        onClick={goToNextSlide}
+      />
     </div>
   );
 };
-
 export default Carousel;
--- front/src/pages/ApartmentPage.js
+++ front/src/pages/ApartmentPage.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import Carousel from "../components/carousel/Carousel";
-// import ApartmentTitle from "../components/apartmentTitle/ApartmentTitle";
-// import Tags from "../components/tags/Tags";
+import Carousel from "../components/carousel/Carousel";
+import ApartmentTitle from "../components/apartmentTitle/ApartmentTitle";
+import Tags from "../components/tags/Tags";
 
 const ApartmentPage = () => {
   const { id } = useParams();
   const [apartmentDetail, setApartmentDetail] = useState(null);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -19,45 +18,38 @@ const ApartmentPage = () => {
         console.error("Error fetching data:", error);
       }
     };
-  
     fetchData();
   }, []);
-
-  const filteredApartments = apartmentDetail.find((item) => item.id === id);
-  console.log("filtre",filteredApartments)
-  const pictures = filteredApartments.pictures;
-  console.log(pictures);
-
-
+  if (apartmentDetail === null) {
+    // Afficher un message de chargement ou un composant de chargement
+    return <div>Loading...</div>;
+  }
+  const filteredApartment = apartmentDetail.find((item) => item.id === id);
+  if (!filteredApartment) {
+    // Gérer le cas où aucun appartement n'est trouvé avec l'ID donné
+    return <div>Apartment not found</div>;
+  }
+  const listPictures = filteredApartment.pictures;
+  console.log("filtre", filteredApartment);
+  console.log(listPictures);
   return (
     <main>
-      {/* <section className="apartment-list">
-        {filteredApartments.map((item) => (
-          <Carousel id={item.id} pictures={item.pictures} />
-        ))}
-      </section> */}
-      {/* <section>
-        <Carousel id ={id} pictures={pictures}/>
-      </section> */}
+      <section className="apartment-list">
+        <Carousel id={id} pictures={listPictures} />
+      </section>
 
-      {/* <section>
-        {filteredApartments.map((item) => (
-          <ApartmentTitle id={item.id} title={item.title} location={item.location} />
-       
-        
-        ))}
+      <section>
+        <ApartmentTitle
+          id={filteredApartment.id}
+          title={filteredApartment.title}
+          location={filteredApartment.location}
+        />
+      </section>
 
-      </section> */}
-      {/* {filteredApartments.map((item)=>(
-        <Tags id={item.id} tags={item.tags}/>
-      ))} */}
-      {/* <div>
-        <Tags id={filteredApartments.id} tags={filteredApartments.tags}/>
-      </div> */}
-
+      <div>
+        <Tags id={filteredApartment.id} tags={filteredApartment.tags} />
+      </div>
     </main>
-
   );
 };
-
 export default ApartmentPage;
