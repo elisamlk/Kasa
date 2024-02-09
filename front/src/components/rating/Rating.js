20ab110ad5789beb16d4deddb97aa07a6caad0f9
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2024-02-09 16:48:16 +0100

    Ajustement des étoiles de notation

--- front/src/components/rating/Rating.js
+++ front/src/components/rating/Rating.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faStarHalfAlt, faStar as faStarEmpty } from '@fortawesome/free-solid-svg-icons';
-import { faStar as faStarOutline } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faStarHalfAlt,
+  faStar as faStarEmpty,
+} from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarOutline } from "@fortawesome/free-regular-svg-icons";
+import "./Rating.scss";
 
 const Rating = (props) => {
-    const rating = props.rating;
+  const rating = props.rating;
   const totalStars = 5;
   const fullStars = Math.floor(rating);
   const hasHalfStar = rating % 1 !== 0;
@@ -16,15 +21,23 @@ const Rating = (props) => {
   }
 
   if (hasHalfStar) {
-    stars.push(<FontAwesomeIcon key="half" icon={faStarHalfAlt} color="#ff6060" />);
+    stars.push(
+      <FontAwesomeIcon key="half" icon={faStarHalfAlt} color="#ff6060" />
+    );
   }
 
   const emptyStars = totalStars - stars.length;
   for (let i = 0; i < emptyStars; i++) {
-    stars.push(<FontAwesomeIcon key={`empty-${i}`} icon={faStarOutline} color="#d3d3d3" />);
+    stars.push(
+      <FontAwesomeIcon
+        key={`empty-${i}`}
+        icon={faStarOutline}
+        color="#d3d3d3"
+      />
+    );
   }
 
-  return <div>{stars}</div>;
+  return <div className="rating">{stars}</div>;
 };
 
 export default Rating;
