bf7c9626bdda30ec89eca283a1d2e7c86b36f61f
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2024-02-09 16:21:38 +0100

    Mise à la ligne du nom de l'hôte

--- front/src/components/host/Host.js
+++ front/src/components/host/Host.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./Host.scss";
 
 const Host = (props) => {
-    return (
-        <div className="host">
-            <h3 className="host-name">{props.name}</h3>
-            <img className="host-img" src={props.picture} alt={props.name} />
-        </div>
-    );
+  const nameWords = props.name.split(" ");
+  const formattedName = `${nameWords[0]}<br />${nameWords.slice(1).join(" ")}`;
+
+  return (
+    <div className="host">
+      <h3 className="host-name" dangerouslySetInnerHTML={{ __html: formattedName }}></h3>
+      <img className="host-img" src={props.picture} alt={props.name} />
+    </div>
+  );
 };
 
 export default Host;
